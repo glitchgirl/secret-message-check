e5d76cb01200ebf4896e3ffdfd711727918f63fa
Morgan Smith <londonspleen@gmail.com>
2026-05-18 11:42:54 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,11 @@
+# takes in the URL
+# check URL is valid 
 
+# retrieve the data
+# don't have to do too much checking here, we can assume the data is formatted correctly based on the instructions
+
+# parse the data
+# data starts at zero and can get infinitely large
+
+# print the secret 
+# basically, each row is 3 values: x,y and what char to print, and when printed together it'll print a letter 

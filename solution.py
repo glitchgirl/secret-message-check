1c97450782599ded86c84aed09993330a66b2fc7
Morgan Smith <londonspleen@gmail.com>
2026-05-18 11:47:06 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,11 @@
+import requests
+
 # takes in the URL
 # check URL is valid 
+
+link = "https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub"
+test = requests.get(link)
+print(test.text)
 
 # retrieve the data
 # don't have to do too much checking here, we can assume the data is formatted correctly based on the instructions
